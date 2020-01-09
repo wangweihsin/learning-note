e9f698ac280f2694152bf5d964796500fbfca794
wangweihsin <55697708+wangweihsin@users.noreply.github.com>
2020-01-09 21:29:39 +0800

    Add files via upload

---
+++ Codesignal/9.mexFunction.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+def mexFunction(s, upperBound):
+    found = -1
+    for i in range(upperBound):
+        if not i in s:
+            found = i
+            break
+    else:
+        return upperBound
+
+    return found
+
